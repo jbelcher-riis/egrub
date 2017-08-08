4b8c7cc4c328419035d68f4b13f36febae6ebac7
Jim Belcher <jbelcher@riis.com>
2017-08-08 18:38:33 -0400

    initial commit

---
+++ src/main/java/com/egrub/egrub/EgrubApplication.java
@@ -0,0 +1,12 @@
+package com.egrub.egrub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgrubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgrubApplication.class, args);
+	}
+}
---
+++ src/test/java/com/egrub/egrub/EgrubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.egrub.egrub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EgrubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
